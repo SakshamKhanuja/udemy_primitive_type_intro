e60f3afb4c1ba3aafc25558eed5acd13b3cdefd5
SakshamKhanuja <ssk@outlook.ie>
2022-07-26 21:07:57 +0530

    Add byte, short, and long data type.

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // 1. int - Integer
+        // 1. int (Primitive type) - Integer (Wrapper Class)
 
         // Stores the minimum valid "int" value.
         int myMinIntValue = Integer.MIN_VALUE;
@@ -16,10 +16,10 @@ public class Main {
         System.out.println("Integer Minimum Value = " + myMinIntValue);
         System.out.println("Integer Maximum Value = " + myMaxIntValue);
 
-        // Adding 1 to "myMaxIntValue".
+        // Adding 1 to "myMaxIntValue" - OVERFLOW
         System.out.println("Busted MAX Value = " + (myMaxIntValue + 1));
 
-        // Adding -1 to "myMinIntValue".
+        // Adding -1 to "myMinIntValue" - UNDERFLOW
         System.out.println("Busted MIN Value = " + (myMinIntValue - 1));
 
         /*
@@ -28,5 +28,53 @@ public class Main {
          * The above statement would result in an error, as the literal value exceeds the maximum range of an "int"
          * type.
          */
+
+        // 2. byte (Primitive type) - Byte (Wrapper Class)
+
+        // Stores minimum and maximum valid "byte" values.
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+
+        // Prints out the minimum and maximum "byte" values.
+        System.out.println("Byte Minimum Value = " + myMinByteValue);
+        System.out.println("Byte Maximum Value = " + myMaxByteValue);
+
+        // 3. short (Primitive type) - Short (Wrapper Class)
+
+        // Stores minimum and maximum valid "short" values.
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+
+        // Prints out the minimum and maximum "short" values.
+        System.out.println("Short Minimum Value = " + myMinShortValue);
+        System.out.println("Short Maximum Value = " + myMaxShortValue);
+
+        // 4. long (Primitive type) - Long (Wrapper Class)
+
+        // Stores minimum and maximum valid "long" values.
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+
+        // Prints out the minimum and maximum "long" values.
+        System.out.println("Long Minimum Value = " + myMinLongValue);
+        System.out.println("Long Maximum Value = " + myMaxLongValue);
+
+        // Appending "L" at the end of a value informs the computer that this value is a "long" literal.
+        long myLongValue = 100L;
+        System.out.println(myLongValue);
+
+        /*
+         * long myLongValue = 100;
+         *
+         * The above statement is valid because an "int" type fits entirely in a "long" datatype due to "long" having
+         * twice the width that of "int"; hence Java will convert the "int" value to "long" automatically.
+         *
+         * long bigLongLiteralValue = 2147483648; IDE Message -> Integer number too large
+         *
+         * The above statement will result in an error because Java still treats the value as an "int" literal that
+         * exceeds its maximum range. Adding "L" at the end of the value will let Java treat it as a "long" literal.
+         */
+        long bigLongLiteralValue = 2147483648L;
+        System.out.println(bigLongLiteralValue);
     }
 }
